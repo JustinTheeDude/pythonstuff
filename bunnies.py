c41140e950f95bfdbbf77e87cbf60516c60948bf
Justin <JustinTheeDude@gmail.com>
2018-06-21 00:16:03 -0700

    added itertools

--- bunnies.py
+++ bunnies.py
@@ -1,14 +1,12 @@
+from itertools import combinations
+
 plates = [1, 8, 8, 2]
 
 def bunnies(plates):
-    if len(plates) < 9:
+    if not len(plates) < 9:
         print 'pls add plates'
-    else:
-        for i in range(0, plates):
-            print([i])
-        plates = sum(plates)
-        if plates % 3 == 0:
-            print plates
-        else:
-            print 'bad'
+
+    for i in combinations(plates, ):
+        print (i)
+
 bunnies(plates)

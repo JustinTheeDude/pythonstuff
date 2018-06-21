3c6070677c4ed9f4550d7839e9dd66e2269dbd5a
Justin <JustinTheeDude@gmail.com>
2018-06-21 00:27:43 -0700

    help pls

--- bunnies.py
+++ bunnies.py
@@ -5,8 +5,8 @@ plates = [1, 8, 8, 2]
 def bunnies(plates):
     if not len(plates) < 9:
         print 'pls add plates'
-
-    for i in combinations(plates, ):
-        print (i)
+    for n in range(1, len(plates)):
+        for i in combinations(plates, 2):
+            print (i)
 
 bunnies(plates)

7df2dbff7f40f6a586807e13b58664ae5cf3990c
Justin <JustinTheeDude@gmail.com>
2018-06-25 20:58:02 -0700

    got the biggest number boys

--- bunnies.py
+++ bunnies.py
@@ -3,13 +3,13 @@ from itertools import combinations
 plates = [1, 8, 8, 2]
 
 def bunnies(plates):
-    if not len(plates) < 9:
+    if not len(plates) <= 9:
         print 'pls add plates'
-    for i in combinations(plates, len(plates)):
-       sortedPlates = sorted(plates, key=int, reverse=True)
-       sumOfPlates = sum(plates)
-       print sortedPlates     
-       if sumOfPlates % 3 == 0:
-           print 'Ay ay captain'
-           
+    for n in range(0, len(plates)+1):
+        for i in combinations(plates, len(plates)):
+           sortedPlates = sorted(plates, key=int, reverse=True)
+           sumOfPlates = sum(plates)
+           if sumOfPlates % 3 == 0:
+               print 'cool'
+    print int(''.join(map(str, sortedPlates)))
 bunnies(plates)

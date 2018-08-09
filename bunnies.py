91d2308d8baac47e4ca9eb2c100ae047a4d6a20b
JustinTheeDude <JustinTheeDude@gmail.com>
2018-08-09 14:43:47 -0700

    aksdfjlkds

--- bunnies.py
+++ bunnies.py
@@ -1,24 +1,38 @@
 from itertools import combinations
-plates = [5,5,3]
+plates = [8, 2, 1, 4, 1, 2, 5]
 
 
 def bunnies(plates):
-    if not len(plates) <= 9:
-        print 'pls add plates'
     for i in combinations(plates, len(plates)):
         sumOfPlates = sum(plates)
-        if sumOfPlates % 3 == 0:
-            print 'good'
         if sumOfPlates % 3 == 1:
-            platelist = [n for n in plates]
-            platelist.pop(platelist.index(-max([-u for u in platelist if u % 3 == 2])))
+            platelist = [n for n in i]
+            try:
+                platelist.pop(platelist.index(min(
+                    [u for u in platelist if u % 3 == 1])))
+            except ValueError:
+                platelist.pop(platelist.index(min(
+                    [u for u in platelist if u % 3 == 2])))
+                platelist.pop(platelist.index(min(
+                    [u for u in platelist if u % 3 == 2])))
             sortedp = sorted(platelist, key=int, reverse=True)
 
         if sumOfPlates % 3 == 2:
             platelist = [n for n in plates]
-            platelist.pop(platelist.index(-max([-u for u in platelist if u % 3 == 2])))
+            try:
+                platelist.pop(platelist.index(min(
+                    [u for u in platelist if u % 3 == 2])))
+            except ValueError:
+                platelist.pop(platelist.index(min(
+                    [u for u in platelist if u % 3 == 1])))
+                platelist.pop(platelist.index(min(
+                    [u for u in platelist if u % 3 == 1])))
             sortedp = sorted(platelist, key=int, reverse=True)
 
-        print int(''.join(map(str, sortedp)))
+        return ''.join(map(str, sortedp))
 
-bunnies(plates)
+
+for count in xrange(10):
+    something = [int(x) for x in list("{}".format(count))]
+    ans = bunnies(plates)
+    print("{} : {}".format(count, ans))

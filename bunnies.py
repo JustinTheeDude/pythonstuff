78a3c894183bea5c6540af07941d4a0086af3515
Justin Teves <jteves@apartments247.com>
2018-08-08 18:02:42 -0700

    kasldf

--- bunnies.py
+++ bunnies.py
@@ -1,19 +1,22 @@
-from itertools import combinations, permutations
-import pprint
-
+from itertools import combinations
 plates = [8, 5, 1, 8, 3, 4]
+
 
 def bunnies(plates):
     if not len(plates) <= 9:
         print 'pls add plates'
-    for i in combinations(plates, len(plates)):
+    for i in xrange(len(plates), -1, -1):
         sortedPlates = sorted(plates, key=int, reverse=True)
+        platestring = ''.join(map(str, sortedPlates))
         sumOfPlates = sum(plates)
-        newPlates = list(permutations(sortedPlates, 2))
-        sumplate = map(sum, newPlates)
-        pprint.pprint(sumplate)
-        pprint.pprint(newPlates)
-        if sumplate % 3 == 1:
-            check(sumplate)
-        print int(''.join(map(str, sortedPlates)))
+        if sumOfPlates % 3 == 0:
+            print 'good'
+        if sumOfPlates % 3 == 2:
+            for comb in combinations(platestring, i):
+                platelist = [n for n in comb if n % 3 == 2]
+                print platelist
+                return
+        # print int(''.join(map(str, sortedPlates)))
+
+
 bunnies(plates)
